e673e9126abfe8758c40bae2e532dccadc6308a8
qiye <1710797241@qq.com>
2023-03-01 14:51:27 +0800

    fix: bug

--- packages/umi3Base/plugins/wujie/src/index.ts
+++ packages/umi3Base/plugins/wujie/src/index.ts
@@ -16,7 +16,7 @@ module.exports = (api: IApi) => {
     },
     enableBy: api.EnableBy.config,
   });
-  api.addRuntimePlugin(() => '@@/plugin-wujie/masterRuntimePlugin');
+  api.addRuntimePlugin(() => '../plugin-wujie/masterRuntimePlugin');
 
   api.onGenerateFiles(() => {
     api.writeTmpFile({
@@ -75,6 +75,53 @@ module.exports = (api: IApi) => {
   }
   
       
+      `,
+    });
+    api.writeTmpFile({
+      path: 'plugin-wujie/masterRuntimePlugin.ts',
+      content: `import { ApplyPluginsType, IRoute, plugin } from 'umi';
+      import { getMasterOptions } from '@@/plugin-wujie/masterOptions';
+      import { getMicroAppRouteComponent } from './getMicroAppRouteComponent';
+      
+      export async function render(oldRender: any) {
+        async function getMasterRuntime() {
+          const config = await plugin.applyPlugins({
+            key: 'wujie',
+            type: ApplyPluginsType.modify,
+            initialValue: {},
+            async: true,
+          });
+          const { master } = config;
+          return master || config;
+        }
+        const config = await getMasterRuntime();
+        const masterOptons = getMasterOptions();
+        console.log('plugin render', masterOptons);
+        oldRender();
+      }
+      
+      export function patchRoutes(originRoute: IRoute) {
+        const { apps = [], masterHistoryType, base } = getMasterOptions();
+      
+        console.log('plugin patchRoutes', originRoute, apps);
+        originRoute?.routes?.forEach((route) => {
+          if (route.microApp) {
+            const curr = apps.find((item) => item.name == route.microApp);
+            route.component = getMicroAppRouteComponent({
+              appName: route.name,
+              base,
+              masterHistoryType,
+              routeProps: { url: curr.entry },
+            });
+          }
+      
+          if (route.routes?.length) {
+            patchRoutes(route);
+          }
+        });
+      
+        return originRoute;
+      }
       `,
     });
   });
